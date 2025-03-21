bdf3bdfa4001c3da9dccd5a2c70eb5e54db6e9bb
Sanjaya2428250037 <sanjaya_2428250037@mhs.mdp.ac.id>
2025-03-21 17:22:04 +0700

    p

---
+++ javascript/script.js
@@ -0,0 +1,43 @@
+//mengubah konten html
+document.getElementById('judul').innerHTML = "<h1> javaCript </h1>";
+
+//variable
+let nama = "Sanjaya";
+let umur = 17;
+document.getElementById('nama').innerHTML = "Nama saya : "+ nama;
+document.getElementById('umur').innerHTML = `umur saya : ${umur} tahun`;
+
+let mahasiswa = ["Christian","Kolin","Savero"];
+console.log(mahasiswa);
+mahasiswa.forEach( (mhs)=> {
+    document.getElementById('mahasiswa').innerHTML += `<li> ${mhs} </li>`;
+})
+
+let nilai = [
+    {nama : "Kelvin" , nilai : "A" },
+    {nama : "Kolin" , nilai : "A" },
+    {nama : "Savero" , nilai : "A" },
+];
+
+console.log(nilai);
+nilai.forEach( (n)=> {
+    document.getElementById('nilai').innerHTML += 
+    `<tr> 
+        <td> ${n.nama} </td>
+        <td> ${n.nilai } </td>
+    </tr>`;
+  
+})
+
+// public Api data gempa BMKG
+fetch('https://data.bmkg.go.id/DataMKG/TEWS/gempaterkini.json')
+.then(respone => respone.json())
+.then( data => {
+    //console.log(data);
+    console.log(data.Infogempa.gempa);
+    data.Infogempa.gempa.forEach( (data) => {
+    document.getElementById('gempa').innerHTML += `<li>
+        ${data.Wilayah} ${data.Tanggal} ${data.Jam} ${data.Potensi}
+    </li>`
+    })
+})
